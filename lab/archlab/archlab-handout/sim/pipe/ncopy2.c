b73918fcb022440f2f8d65140bae2895f28f1ee3
liuxiao <liuxiao@oro.com>
2022-08-09 11:13:40 +0800

    upgrade archlab

--- lab/archlab/archlab-handout/sim/pipe/ncopy2.c
+++ lab/archlab/archlab-handout/sim/pipe/ncopy2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#define LEN 8
+#define LEN 64
 
-int src[LEN], dst[LEN];
-
-int loopCount = 0;
+int loopCount;
 
 int ncopy(int *src, int *dst, int len)
 {
   int i, val;
   int count = 0;
-  int k = 4;
+  int k = 8;
+  int subK = 3;
+  loopCount = 0;
 
   // for(i = len - k; i >= 0; i -= k) {
   //   val = src[i];
@@ -33,22 +33,55 @@ int ncopy(int *src, int *dst, int len)
     val = src[i + 3];
     dst[i + 3] = val;
     if(val > 0) count++;
-    // val = src[i + 4];
-    // dst[i + 4] = val;
-    // if(val > 0) count++;
-    // val = src[i + 5];
-    // dst[i + 5] = val;
-    // if(val > 0) count++;
+    val = src[i + 4];
+    dst[i + 4] = val;
+    if(val > 0) count++;
+    val = src[i + 5];
+    dst[i + 5] = val;
+    if(val > 0) count++;
+    val = src[i + 6];
+    dst[i + 6] = val;
+    if(val > 0) count++;
+    val = src[i + 7];
+    dst[i + 7] = val;
+    if(val > 0) count++;
+    val = src[i + 8];
 
     loopCount++;
   }
 
-  for(k += i - 1; k >= 0; k--) {
-    val = src[k];
-    dst[k] = val;
+  for(i = k + i - subK; i >= 0; i -= subK) {
+    val = src[i];
+    dst[i] = val;
+    if(val > 0) count++;
+
+    val = src[i + 1];
+    dst[i + 1] = val;
+    if(val > 0) count++;
+    val = src[i + 2];
+    dst[i + 2] = val;
+    if(val > 0) count++;
+
+    loopCount++;
+  }
+
+  for(subK += i - 1; subK >= 0; subK--) {
+    val = src[subK];
+    dst[subK] = val;
     if(val > 0) count++;
     loopCount++;
   }
+
+  // printf("K is: %d\n", k + i - 1);
+  // printf("K is: %d\n", k + i - subK);
+  //
+  //
+  // for(k += i - 1; k >= 0; k--) {
+  //   val = src[k];
+  //   dst[k] = val;
+  //   if(val > 0) count++;
+  //   loopCount++;
+  // }
 
   return count;
 }
@@ -56,14 +89,21 @@ int ncopy(int *src, int *dst, int len)
 
 int main() {
   int i, count;
+  int len;
 
-  for (i = 0; i < LEN; i++)
-     src[i]= i + 1;
-  count = ncopy(src, dst, LEN);
-  for (i = 0; i < LEN; i++)
-    printf ("%d ", dst[i]);
-  printf ("\ncount = %d\n", count);
-  printf ("loopCount = %d\n", loopCount);
+  for(len = 0; len < LEN; len++) {
+    printf("len: %d\n", len);
+    int src[len], dst[len];
+
+    for (i = 0; i < len; i++)
+       src[i]= i + 1;
+    count = ncopy(src, dst, len);
+    for (i = 0; i < len; i++)
+      printf ("%d ", dst[i]);
+    printf ("\t\t\ncount = %d\n", count);
+    printf ("\t\tloopCount = %d\n", loopCount);
+    printf ("--------------------------------\n");
+  }
 
   return 0;
 }
